2ad04de84e44c1f0f0a00a077efdf71ef204df6c
dion edge <158282973+wrenchchat@users.noreply.github.com>
2024-10-22 11:17:03 -0500

    Delete queries directory

--- queries/10_create_predict_view.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW `dion-miguel-0001.wc_ds_001.tickets_predict_view`
-AS
-SELECT *
-FROM `dion-miguel-0001.wc_ds_001.tickets`
---WHERE Agent_Users_Name = "Dion Edge" 
-WHERE Model_DS_Type IS NULL
